f99e628a9db27d0cff6c81b859033b636eb6a955
Sanjay <sanjaykirupaharan21@gmail.com>
2023-01-14 11:14:02 +0530

    feat: add about and team section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,5 @@
+
 function myFunction() {
     document.getElementById("nav-links").classList.toggle("active");
 }
 
-// const toggleButton = document.getElementsById("toggle-button")[0]
-const navlinks = document.getElementsById("nav-links")[0]
-
-// toggleButton.addEventListener("click", () => {
-//     navlinks.classList.toggle("active")
-// })
